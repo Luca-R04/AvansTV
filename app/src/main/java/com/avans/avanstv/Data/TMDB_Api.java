9a5d23d6e0fe8930576d0b048571035abca0be3f
lauwIV <64641417+lauwIV@users.noreply.github.com>
2022-03-27 13:28:57 +0200

    ++API && Movie class

---
+++ app/src/main/java/com/avans/avanstv/Data/TMDB_Api.java
@@ -0,0 +1,23 @@
+package com.avans.avanstv.Data;
+
+import com.avans.avanstv.Domain.Movie;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public interface TMDB_Api {
+
+    /** Authentication **/
+
+    /** Movies **/
+
+    //Get movie by id
+    @GET("movie/{movie_id}?api_key={api_key}")
+    Call<Movie> getMovieID(@Path("movie_id") int movie_id, @Path("api_key") String api_key);
+
+    //Get movie by id + language
+    @GET("movie/{movie_id}?api_key={api_key}&language={language}")
+    Call<Movie> getMovieID(@Path("movie_id") int movie_id, @Path("api_key") String api_key, @Path("Language") String language);
+
+}
