c70c2be9941592fd72ac6c19ff75c9805e4c75d7
lauwIV <64641417+lauwIV@users.noreply.github.com>
2022-03-30 14:04:57 +0200

    Fragment: added controller for nav_bar

--- app/src/main/java/com/avans/avanstv/Presentation/MainActivity.java
+++ app/src/main/java/com/avans/avanstv/Presentation/MainActivity.java
@@ -1,10 +1,14 @@
 package com.avans.avanstv.Presentation;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
 import com.avans.avanstv.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +16,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Bottom navigation
+        //initiate controller for bottom navigation to witch between fragments
+        BottomNavigationView bottomNavigationView = findViewById(R.id.btm_nav);
+        final NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.host_fragment);
+        final NavController navController = navHostFragment.getNavController();
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
     }
 }
