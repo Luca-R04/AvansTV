64c29b8e8e5a183ab2cace2c04b1be9ac0a5dc5d
Martijn <martijnschermers2@gmail.com>
2022-03-30 18:21:47 +0200

    Changed method parameters to @Query

--- app/src/main/java/com/avans/avanstv/Data/TMDB_Api.java
+++ app/src/main/java/com/avans/avanstv/Data/TMDB_Api.java
@@ -6,20 +6,21 @@ import com.avans.avanstv.Domain.MovieResponse;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface TMDB_Api {
 
     /** Authentication **/
 
     /** Movies **/
-    @GET("movie/popular?api_key={api_key}")
-    Call<MovieResponse> getPopularMovies(@Path("api_key") String api_key);
+    @GET("movie/popular")
+    Call<MovieResponse> getPopularMovies(@Query("api_key") String api_key);
 
     //Get movie by id
-    @GET("movie/{movie_id}?api_key={api_key}")
-    Call<Movie> getMovieID(@Path("movie_id") int movie_id, @Path("api_key") String api_key);
+    @GET("movie/{movie_id}")
+    Call<Movie> getMovieID(@Path("movie_id") int movie_id, @Query("api_key") String api_key);
 
     //Get movie by id + language
-    @GET("movie/{movie_id}?api_key={api_key}&language={language}")
-    Call<Movie> getMovieID(@Path("movie_id") int movie_id, @Path("api_key") String api_key, @Path("Language") String language);
+    @GET("movie/{movie_id}?language={language}")
+    Call<Movie> getMovieID(@Path("movie_id") int movie_id, @Query("api_key") String api_key, @Path("Language") String language);
 }
