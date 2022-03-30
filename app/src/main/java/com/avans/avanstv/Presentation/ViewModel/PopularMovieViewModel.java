dfa43faf9b71ef2a111a1a23e740b116224386b3
lauwIV <64641417+lauwIV@users.noreply.github.com>
2022-03-30 16:19:32 +0200

    viewModel: PopularMovieViewModel created

--- app/src/main/java/com/avans/avanstv/Presentation/ViewModel/PopularMovieViewModel.java
+++ app/src/main/java/com/avans/avanstv/Presentation/ViewModel/PopularMovieViewModel.java
@@ -4,14 +4,24 @@ import android.app.Application;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 
 import com.avans.avanstv.Data.MovieRepository;
+import com.avans.avanstv.Domain.Movie;
+
+import java.util.List;
 
 public class PopularMovieViewModel extends AndroidViewModel {
+    private LiveData<List<Movie>> mMovie;
     private MovieRepository mMovieRepository;
 
     public PopularMovieViewModel(@NonNull Application application) {
         super(application);
+        mMovieRepository.getMovies();
+    }
 
+    public LiveData<List<Movie>> getMeals() {
+        return mMovie;
     }
 }
