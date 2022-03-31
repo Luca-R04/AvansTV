128185d0b7667336c6d6a3ae207f34ae7174e2a6
Luca-R04 <Lucarinck04@gmail.com>
2022-03-31 13:55:15 +0200

    Merge branch 'main' of https://github.com/Luca-R04/AvansTV

--- app/src/main/java/com/avans/avanstv/Presentation/HomeFragment.java
+++ app/src/main/java/com/avans/avanstv/Presentation/HomeFragment.java
@@ -3,29 +3,29 @@ package com.avans.avanstv.Presentation;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.Toast;
 
+import com.avans.avanstv.Data.MovieRepository;
 import com.avans.avanstv.Domain.Movie;
 import com.avans.avanstv.Presentation.ViewModel.PopularMovieViewModel;
 import com.avans.avanstv.R;
 import com.bumptech.glide.Glide;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class HomeFragment extends Fragment {
     private PopularMovieViewModel popularMovieViewModel;
-    private List<Movie> mMovie = new ArrayList<>();
+//    private List<Movie> mMovie;
     private RecyclerView mPopularRecyclerView;
     private MovieAdapter movieAdapter;
+//    MovieRepository movieRepository = MovieRepository.getInstance();
 
     public HomeFragment() {
         // Required empty public constructor
@@ -34,13 +34,15 @@ public class HomeFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("HomeFragment", "OnCreate is called");
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View homeView = inflater.inflate(R.layout.fragment_home, container, false);
-
+//        mMovie = movieRepository.getMovies();
+//        setRandomMovie();
 
 //        //listener for change in the dataset in the LiveView
 //        //updates the adapter when data is loaded
@@ -58,15 +60,19 @@ public class HomeFragment extends Fragment {
         return homeView;
     }
 
-    private void setRandomMovie(List<Movie> movies) {
-        int random = new Random().nextInt(movies.size());
-        ImageView suggestedMealView = getActivity().findViewById(R.id.img_randomMeal);
+    public void setRandomMovie(List<Movie> movies) {
+        if (movies != null) {
+            int random = new Random().nextInt(movies.size());
+            ImageView suggestedMealView = getActivity().findViewById(R.id.img_randomMeal);
 
-        Glide
-                .with(getContext())
-                .load("https://image.tmdb.org" + movies.get(random).getPoster_path())
-                .centerCrop()
-                .into(suggestedMealView);
+            Glide
+                    .with(getContext())
+                    .load("https://image.tmdb.org" + movies.get(random).getPoster_path())
+                    .centerCrop()
+                    .into(suggestedMealView);
+        } else {
+            Log.d("HomeFragment", "The movie list is empty!");
+        }
 
     }
 }
--- app/src/main/java/com/avans/avanstv/Presentation/ViewModel/PopularMovieViewModel.java
+++ app/src/main/java/com/avans/avanstv/Presentation/ViewModel/PopularMovieViewModel.java
@@ -5,7 +5,6 @@ import android.app.Application;
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
 
 import com.avans.avanstv.Data.MovieRepository;
 import com.avans.avanstv.Domain.Movie;
