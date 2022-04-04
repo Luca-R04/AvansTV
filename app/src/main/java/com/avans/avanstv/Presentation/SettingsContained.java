9332ddf330340200d531062e30eeb5144628ccc4
lauwIV <64641417+lauwIV@users.noreply.github.com>
2022-04-04 18:12:08 +0200

    SettingsContained: Created new files

---
+++ app/src/main/java/com/avans/avanstv/Presentation/SettingsContained.java
@@ -0,0 +1,31 @@
+package com.avans.avanstv.Presentation;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.avans.avanstv.R;
+
+
+public class SettingsContained extends Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_settings_contained, container, false);
+
+
+        return view;
+    }
+}
