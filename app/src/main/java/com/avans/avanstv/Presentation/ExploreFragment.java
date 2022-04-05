ade153628fc0b4681ddee552cec15c2fcb1be4e8
Martijn <martijnschermers2@gmail.com>
2022-04-05 19:43:46 +0200

    Search function bugs kinda fixed

--- app/src/main/java/com/avans/avanstv/Presentation/ExploreFragment.java
+++ app/src/main/java/com/avans/avanstv/Presentation/ExploreFragment.java
@@ -1,5 +1,6 @@
 package com.avans.avanstv.Presentation;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -11,6 +12,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.SearchView;
 
 import com.avans.avanstv.Presentation.Adapters.MovieExploreAdapter;
@@ -27,8 +29,7 @@ public class ExploreFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View exploreView = inflater.inflate(R.layout.fragment_explore, container, false);
         PopularMovieViewModel mPopularMovieViewModel = ViewModelProviders.of(this).get(PopularMovieViewModel.class);
         SearchMovieViewModel mSearchViewModel = ViewModelProviders.of(this).get(SearchMovieViewModel.class);
@@ -39,41 +40,35 @@ public class ExploreFragment extends Fragment {
         exploreRecyclerView.setLayoutManager(new LinearLayoutManager(this.getContext()));
         exploreRecyclerView.setAdapter(movieAdapter);
 
-        //updates adapter
-        mPopularMovieViewModel.getAllMovies().observe(this.getViewLifecycleOwner(),
-                movieAdapter::setMovies);
-
         SearchView searchView = exploreView.findViewById(R.id.explore_search);
         searchView.setSubmitButtonEnabled(true);
         searchView.setQueryHint(getString(R.string.query_hint));
-
-
-
+        searchView.setIconifiedByDefault(false);
 
         //QueryTextListener on the searchView;
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String s) {
-                mSearchViewModel.setMovie(searchView.getQuery().toString());
-                //updates adapter
-                mSearchViewModel.getAllMovies().observe(getViewLifecycleOwner(),
-                        movieAdapter::setMovies);
+                InputMethodManager imm = (InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(exploreView.getWindowToken(), 0);
+
+                Log.i(TAG, "onQueryTextSubmit");
+
+                if (!searchView.getQuery().toString().equals("")) {
+                    mSearchViewModel.setMovie(searchView.getQuery().toString());
+                }
                 return false;
             }
 
             @Override
             public boolean onQueryTextChange(String s) {
-                Log.d(TAG, "The textfield now says:" + searchView.getQuery().toString());
-                //updates adapter
-                mSearchViewModel.getAllMovies().observe(getViewLifecycleOwner(),
-                        movieAdapter::setMovies);
+                Log.d(TAG, "The text field now says:" + s);
                 return false;
             }
         });
 
         //updates adapter
-        mSearchViewModel.getAllMovies().observe(getViewLifecycleOwner(),
-                movieAdapter::setMovies);
+        mSearchViewModel.getAllMovies().observe(getViewLifecycleOwner(), movieAdapter::setMovies);
 
         return exploreView;
     }
