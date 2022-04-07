b64cbd8b7426d7968bfbf23fc3626316d48ca28a
lauwIV <64641417+lauwIV@users.noreply.github.com>
2022-04-07 14:57:13 +0200

    FilterFragment: created

---
+++ app/src/main/java/com/avans/avanstv/Presentation/ViewModel/FilterContained.java
@@ -0,0 +1,44 @@
+package com.avans.avanstv.Presentation.ViewModel;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.avans.avanstv.Presentation.FilterFragment;
+import com.avans.avanstv.Presentation.SettingsFragment;
+import com.avans.avanstv.R;
+import com.google.android.material.button.MaterialButton;
+
+public class FilterContained extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_settings_contained, container, false);
+
+        getChildFragmentManager().beginTransaction().replace(R.id.rootPreferenceHolder, new FilterFragment()).commit();
+
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        MaterialButton backArrow = view.findViewById(R.id.settings_btn_back);
+
+        backArrow.setOnClickListener(view1 -> Navigation.findNavController(view1).navigate(R.id.action_settingsContained_to_profileFragment));
+    }
+}
